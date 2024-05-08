135fb754b3a59a4d12208cae655ef9dc3c6e6584
Almamy_IT_Training <Almamy_IT_Training@Almamys-MBP.home>
2024-05-08 15:58:20 -0400

    adding DAo and service files

--- src/main/java/projects/ProjectsApp.java
+++ src/main/java/projects/ProjectsApp.java
@@ -1,14 +1,151 @@
 package projects;
 
-import java.sql.Connection;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
 
-import projects.dao.DbConnection;
+import projects.entity.Project;
+import projects.exception.DbException;
+import projects.service.ProjectService;
 
 public class ProjectsApp {
+	
+	Scanner scanner = new Scanner(System.in);
+	ProjectService projectService = new ProjectService();
+	
+	// @formatter:off
+	private List<String> operations = List.of(
+			"1) Add a project"
+			);
+	// @formatter:on
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Connection conn = DbConnection.getConnection();
+		
+		new ProjectsApp().processUserSelections();
+			
+	}
+	
+	private void processUserSelections() {
+		// TODO Auto-generated method stub
+		
+		boolean done = false;
+		
+		while(!done) {
+			
+			try {
+				int selection = getUserSelection();
+				
+				switch(selection) {
+				case -1: 
+					done = exitMenu();
+					break;
+					
+				case 1: 
+					createProject();
+					break;
+					
+				default:
+					System.out.println("\n" + selection + " is not a valid selection. Try again.");
+				}
+			}
+			catch(Exception e) {
+				System.out.println("\n Error: " + e + " Try again.");
+			}
+		}
+		
+	}
+
+	private boolean exitMenu() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Exiting the menu.");
+		return true;
+	}
+
+	private void createProject() {
+		// TODO Auto-generated method stub
+		
+		String projectName = getStringInput("Enter the project name ");
+		BigDecimal estimatedHours = getDecimalInput("Enter the estimated hours ");
+		BigDecimal actualHours = getDecimalInput("Enter the actual hours");
+		Integer difficulty = getIntInput("Enter the project difficulty (1 - 5) ");
+		String notes = getStringInput("Enter the project notes ");
+		
+		Project project = new Project();
+		
+		project.setProjectName(projectName);
+		project.setEstimatedHours(estimatedHours);
+		project.setActualHours(actualHours);
+		project.setDifficulty(difficulty);
+		project.setNotes(notes);
+		
+		Project dbProject = projectService.addProject(project);
+		System.out.println("You have successfuly created a project: " + dbProject);
+		
+	}
+
+	private BigDecimal getDecimalInput(String prompt) {
+		// TODO Auto-generated method stub
+		
+		String input = getStringInput(prompt);
+		
+		if (Objects.isNull(input)) {
+			return null;
+		}
+		
+		try {
+			return new BigDecimal(input).setScale(2);
+		}
+		catch (NumberFormatException e) {
+			throw new DbException(input + " is not a valid decimal number.");
+		}
+		
+	}
+
+	private int getUserSelection() {
+		// TODO Auto-generated method stub
+		
+		printOperations();
+		Integer input = getIntInput("Enter a menu selection ");
+		
+		return Objects.isNull(input) ? -1 : input;
+	}
+
+	private Integer getIntInput(String prompt) {
+		// TODO Auto-generated method stub
+		
+		String input = getStringInput(prompt);
+		
+		if (Objects.isNull(input)) {
+			return null;
+		}
+		
+		try {
+			return Integer.valueOf(input);
+		}
+		catch (NumberFormatException e) {
+			throw new DbException(input + " is not a valid number.");
+		}
+		
+	}
+
+	private String getStringInput(String prompt) {
+		// TODO Auto-generated method stub
+		
+		System.out.print(prompt + ": ");
+		String input = scanner.nextLine();
+		
+		return input.isBlank() ? null : input.trim();
+	}
+
+	private void printOperations() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("\n These are the available selections. Press the enter key to quit: ");
+		
+		operations.forEach(line -> System.out.println(" " + line));
 	}
 
 }
